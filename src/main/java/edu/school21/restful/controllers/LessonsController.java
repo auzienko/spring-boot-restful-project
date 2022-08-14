20720dd53d7f12ea6e10e2c02433a958fa0c07cf
Aleksandr Uzienko <a.uzienko@gmail.com>
2022-08-14 20:01:58 +0300

    add boilerplate controllers

---
+++ src/main/java/edu/school21/restful/controllers/LessonsController.java
@@ -0,0 +1,9 @@
+package edu.school21.restful.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lessons/")
+public class LessonsController {
+}
