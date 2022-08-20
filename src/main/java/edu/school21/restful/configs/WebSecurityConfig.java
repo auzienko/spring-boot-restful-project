116422a898695a8ac66b6511c020fe9525017948
Aleksandr Uzienko <a.uzienko@gmail.com>
2022-08-20 11:45:06 +0300

    Добавил конфиги для отображения swagger ui и возможности подергать get запросы

---
+++ src/main/java/edu/school21/restful/configs/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package edu.school21.restful.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder bCryptEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/swagger-ui/**", "/v3/api-docs/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/users/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
