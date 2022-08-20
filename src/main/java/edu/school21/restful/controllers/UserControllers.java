2513addc513396efde536dfb58472a5820120f59
Aleksandr Uzienko <a.uzienko@gmail.com>
2022-08-20 11:46:31 +0300

    Добавил аннотации разметки для отображения в swagger ui

--- src/main/java/edu/school21/restful/controllers/UserControllers.java
+++ src/main/java/edu/school21/restful/controllers/UserControllers.java
@@ -2,6 +2,8 @@ package edu.school21.restful.controllers;
 
 import edu.school21.restful.models.User;
 import edu.school21.restful.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,36 +13,53 @@ import java.util.Set;
 
 @AllArgsConstructor
 @RestController
+@Tag(name = "users-controller", description = "Users Controller")
 @RequestMapping("/users/")
 public class UserControllers {
 
     private final UserService userService;
+
+    @Operation(
+            summary = "getAllUsers",
+            description = "Method returns all users"
+    )
     @GetMapping(produces = "application/json")
-    public Set<User> getAll(){
+    public Set<User> getAll() {
         Set<User> u = userService.findAll();
         return userService.findAll();
     }
 
+    @Operation(
+            summary = "addNewUser",
+            description = "Method adds a user"
+    )
     @PostMapping()
-    public String addUser(@RequestBody User user){
+    public String addUser(@RequestBody User user) {
         userService.save(user);
         return "User " + user.getLogin() + " saved";
     }
 
+    @Operation(
+            summary = "updateUser",
+            description = "Method edits a user"
+    )
     @PutMapping({"{user_id}"})
-    public ResponseEntity<?> updateUser(@RequestBody User entity,@PathVariable Long user_id){
-        User user =  userService.findById(user_id);                                                //if not found throw exception
+    public ResponseEntity<?> updateUser(@RequestBody User entity, @PathVariable Long user_id) {
+        User user = userService.findById(user_id);
+        //todo if not found throw exception
         entity.setId(user_id);
         userService.save(entity);
-        return new  ResponseEntity<>("User " + user_id + " updated", HttpStatus.OK);
+        return new ResponseEntity<>("User " + user_id + " updated", HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "deleteUser",
+            description = "Method deletes a user"
+    )
     @DeleteMapping({"{user_id}"})
-    public ResponseEntity<?> deleteUser(@PathVariable Long user_id){
+    public ResponseEntity<?> deleteUser(@PathVariable Long user_id) {
         User user = userService.findById(user_id);
         userService.delete(user);
-        return new  ResponseEntity<>("User " + user_id + " deleted", HttpStatus.OK);
+        return new ResponseEntity<>("User " + user_id + " deleted", HttpStatus.OK);
     }
-
-
 }
